a011da9f99de1515eb513de94c413ca5d18629ec
Leandro Borges <lnjaine@gmail.com>
2015-08-25 16:49:30 -0300

    luta

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825185705) do
+ActiveRecord::Schema.define(version: 20150825190242) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
