78d264f5fc684b8ac8693b8120e6eca7a4d66424
Leandro Borges <lnjaine@gmail.com>
2015-08-26 18:01:46 -0300

    autenticado

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do 
+	config.action_mailer.delivery_method = :smtp
+
+	config.action_mailer.smtp_settings = { 
+		address: "smtp.gmail.com",
+		port: 587,
+		authentication: "plain",
+		user_name: "lnjaine@gmail.com", 
+		password: "1248cobra", 
+		enable_starttls_auto: true
+	}
+end
